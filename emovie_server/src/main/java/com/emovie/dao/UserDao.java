845aca71d4c1804955377943ba85f2854402900c
HuangZL20231010 <HuangZL1010@aliyun.com>
2023-04-15 21:14:12 +0800

    改bug

--- emovie_server/src/main/java/com/emovie/dao/UserDao.java
+++ emovie_server/src/main/java/com/emovie/dao/UserDao.java
@@ -17,8 +17,9 @@ public interface UserDao {
     @Select("select * from user where telephone=#{telephone} and password=#{password}")
     public User getUserByTelephoneAndPassword(String telephone, String password);
 
-    @Update("update user set username=#{username},telephone=#{telephone},password=#{password},token=#{token}")
+    @Update("update user set username=#{username},telephone=#{telephone},password=#{password}")
     void updateUser(User user);
+
     @Insert("insert into user (username,password,telephone) values (#{username},#{password},#{telephone})" )
     int register(User user);
 
