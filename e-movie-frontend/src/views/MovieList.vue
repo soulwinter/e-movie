ad7371b35663b5138a71e37ce0039bdd3c41a206
soulwinter <1127907836@qq.com>
2023-05-25 22:40:55 +0800

    add seachbar

--- e-movie-frontend/src/views/MovieList.vue
+++ e-movie-frontend/src/views/MovieList.vue
@@ -1,13 +1,36 @@
 <template>
-    <HeadComponent />
-    <div class="movie-list">
-      <router-link
+  <HeadComponent />
+
+  <!-- 搜索栏 -->
+  <div class="search-bar">
+    <input
+      class="search-input"
+      v-model="searchString"
+      @input="searchRecommendation"
+      type="text"
+      placeholder="搜索电影"
+    />
+    <div class="dropdown" v-if="recommendations.length">
+      <div
+        v-for="item in recommendations"
+        :key="item.id"
+        class="dropdown-item"
+        @click="navigateToMovie(item.id)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+  </div>
+
+  <!-- 电影列表 -->
+  <div class="movie-list">
+    <router-link
       v-for="movie in movies"
       :key="movie.id"
       :to="{ name: 'MovieDetails', params: { id: movie.id } }"
       class="movie-link"
     >
-      <div class="movie-card" v-for="movie in movies" :key="movie.id">
+      <div class="movie-card">
         <h1 class="movie-title">{{ movie.title }}</h1>
         <p class="movie-overview">{{ movie.overview }}</p>
         <div class="info-container">
@@ -21,72 +44,135 @@
           <div class="info-badge" v-if="movie.budget !== 0">${{ movie.budget }} 预算</div>
           <div class="info-badge" v-if="movie.revenue !== 0">${{ movie.revenue }} 票房收入</div>
         </div>
-      
       </div>
     </router-link>
-    </div>
-    <button class="pagination-btn" @click="loadMoreMovies">加载更多电影</button>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import { ref, onMounted } from 'vue';
-  import HeadComponent from '@/components/HeadComponent.vue';
-  
-  export default {
-    name: 'MovieList',
-    components: {
-        HeadComponent,
-    },
-    setup() {
-      const movies = ref([]);
-      let currentPage = 1;
-      const perPage = 10;
-  
-      const fetchMovies = async (page = 1, perPage = 10) => {
-        try {
-          const response = await axios.get(`http://localhost:8081/movie/listInfo/${page}/${perPage}`);
-          if (response.data.success) {
-            movies.value = [...movies.value, ...response.data.data];
-          }
-        } catch (error) {
-          console.error('Error fetching movies:', error);
+  </div>
+
+  <button class="pagination-btn" @click="loadMoreMovies">加载更多电影</button>
+</template>
+
+<script>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import HeadComponent from '@/components/HeadComponent.vue';
+
+export default {
+  name: 'MovieList',
+  components: {
+    HeadComponent,
+  },
+  setup() {
+    const movies = ref([]);
+    let currentPage = 1;
+    const perPage = 10;
+
+    const searchString = ref('');
+    const recommendations = ref([]);
+
+    const fetchMovies = async (page = 1, perPage = 10) => {
+      try {
+        const response = await axios.get(`http://localhost:8081/movie/listInfo/${page}/${perPage}`);
+        if (response.data.success) {
+          movies.value = [...movies.value, ...response.data.data];
         }
-      };
-  
-      const loadMoreMovies = () => {
-        currentPage++;
-        fetchMovies(currentPage, perPage);
-      };
-  
-      onMounted(() => {
-        fetchMovies(currentPage, perPage);
-      });
-  
-      return {
-        movies,
-        loadMoreMovies
-      };
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    const searchRecommendation = async () => {
+      try {
+        const response = await axios.post('http://localhost:8081/movie/searchRecommendation', {
+          movieInfoString: searchString.value
+        });
+        if (response.data.success) {
+          recommendations.value = response.data.data;
+        }
+      } catch (error) {
+        console.error('Error fetching search recommendations:', error);
+      }
+    };
+
+
+    const loadMoreMovies = () => {
+      currentPage++;
+      fetchMovies(currentPage, perPage);
+    };
+
+    const navigateToMovie = (id) => {
+      this.$router.push({ name: 'MovieDetails', params: { id: id } });
+    };
+
+    onMounted(() => {
+      fetchMovies(currentPage, perPage);
+    });
+
+    return {
+      movies,
+      loadMoreMovies,
+      searchString,
+      searchRecommendation,
+      recommendations,
+      navigateToMovie
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.scrollHandler);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.scrollHandler);
+  },
+  methods: {
+    scrollHandler() {
+      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 300) {
+        this.loadMoreMovies();
+      }
     },
-    mounted() {
-      window.addEventListener("scroll", this.scrollHandler);
-    },
-    beforeUnmount() {
-      window.removeEventListener("scroll", this.scrollHandler);
-    },
-    methods: {
-        scrollHandler() {
-            const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-            if (scrollTop + clientHeight >= scrollHeight - 300) {
-                this.loadMoreMovies();
-            }
-      },
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .movie-list {
+  }
+};
+</script>
+
+<style scoped>
+/* Search Bar */
+.search-bar {
+  max-width: 800px;
+  width: 100%;
+  padding: 20px 30px;
+  margin: 0 auto 100px;  /* 空出300px */
+  border-radius: 4px;
+  position: relative;
+}
+
+.search-input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ccc;  /* 灰色边框 */
+}
+
+.dropdown {
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  left: 0;
+  background: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  z-index: 9999;
+}
+
+.dropdown-item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.dropdown-item:hover {
+  background-color: #f2f2f2;
+}
+
+ .movie-list {
     column-count: 3;
     column-gap: 70px;
     max-width: 1280px;
@@ -111,27 +197,27 @@
   .movie-card:hover {
     transform: scale(1.05);
   }
-  
+
   .movie-title {
     font-size: 30px;
     font-weight: bold;
     color: #000;
     margin-bottom: 10px;
   }
-  
+
   .movie-overview {
     font-size: 15px;
     color: #777;
     text-align: left;
     margin-bottom: 10px;
   }
-  
+
   .info-container {
     display: flex;
     flex-wrap: wrap;
     gap: 5px;
   }
-  
+
   .info-badge {
     border: 1px solid #ccc;
     border-radius: 12px;
@@ -139,11 +225,11 @@
     font-size: 12px;
     color: #000;
   }
-  
+
   .adult-badge {
     background-color: #f44336;
   }
-  
+
   .non-adult-badge {
     background-color: #4caf50;
   }
@@ -163,4 +249,3 @@
     background-color: #3079ED;
   }
 </style>
-  
