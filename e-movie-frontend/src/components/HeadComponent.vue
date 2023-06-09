6bacf42bae71b528cfc309f83019adda3657ddb2
soulwinter <1127907836@qq.com>
2023-06-09 20:20:10 +0800

    Add log-out button

--- e-movie-frontend/src/components/HeadComponent.vue
+++ e-movie-frontend/src/components/HeadComponent.vue
@@ -1,31 +1,52 @@
 <template>
-    <header class="header">
-      <h1 class="title">e-movie Movie Information Retrieval System</h1>
-    </header>
-  </template>
+  <header class="header">
+    <h1 class="title">e-movie Movie Information Retrieval System</h1>
+    <router-link v-if="!isLoginPage" to="/login" class="logout-button">登出</router-link>
+  </header>
+</template>
+
   
-  <script>
-  export default {
-    name: 'HeadComponent',
-  };
-  </script>
+<script>
+export default {
+  name: 'HeadComponent',
+  data() {
+    return {
+      isLoginPage: false,
+    };
+  },
+  created() {
+    this.isLoginPage = this.$route.path === '/login';
+  },
+};
+</script>
   
-  <style scoped>
-  .header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #2196f3;
-    height: 60px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .title {
-    font-family: 'Roboto', sans-serif;
-    font-size: 24px;
-    font-weight: bold;
-    color: #ffffff;
-    margin: 0;
-  }
-  </style>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2196f3;
+  height: 60px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.title {
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  color: #ffffff;
+  margin: 0;
+}
+
+.logout-button {
+  color: #ffffff;
+  border: none;
+  background: none;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 10px;
+  /* display: none; */
+}
+</style>
   
